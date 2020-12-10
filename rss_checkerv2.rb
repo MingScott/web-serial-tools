b8989a68f215dde51a72858edfca8163b9d1affb
Ming Scott <ming.scott@gmail.com>
2020-12-10 01:45:50 -0400

    Added more warnings to RSS cycle

--- rss_checkerv2.rb
+++ rss_checkerv2.rb
@@ -88,14 +88,16 @@ def download_feeds(furlhash) #Hash of name=>feed url become hash of name=>feed
 			begin
 				@feedhash[key] = RssFeed::Feed.new(furlhash[key]).to_a_of_h
 			rescue OpenURI::HTTPError => feed_error
-				if feed_error.io.status[0] == "304"
+				warn("Error - Accessing Cached Data")
+				if @oldfeedhash.has_key?(key)
 					@feedhash[key] = @oldfeedhash[key]
 				else
-					raise "Uncaught HTTPError"
+					raise "HTTP error and no cached feed to fallback to"
 				end
 			end
 		end
-	rescue
+	rescue => outer_feed_error
+		warn outer_feed_error
         warn "Unable to download feeds \t[#{Time.now.inspect}]"
         sleep 10
         unless @single
