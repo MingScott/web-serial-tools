7467b81da2ab5e1415ff1bd3a47f16db70151b93
Ming Scott <ming.scott@gmail.com>
2020-12-10 17:29:12 -0400

    formatting change

--- rss_checkerv2.rb
+++ rss_checkerv2.rb
@@ -155,7 +155,7 @@ def populate_document(chaps)
 		@toc << "\t<li><a href=\"##{@chapid}\">#{@chaptitle}</a></li><br>\n"
 		@output << "<h1 class=\"chapter\" id=\"#{@chapid}\">#{@chaptitle}</h1>\n"
 		@output << "<i>" + chaph["date"] + "</i><br>\n"
-		@output << "<a href=\"#{chaph["url"]}\">Webpage</a>"
+		@output << "<a href=\"#{chaph["url"]}\">Webpage</a>\n"
 		@output << "<i>#{@authorstring}</i><br>" unless @authorstring.empty?
 		@output << chap.text + "\n"
 		
