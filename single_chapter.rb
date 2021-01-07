f601197d38aae9f1d933c4cdcea0f80b5471c21d
Ming Scott <ming.scott@gmail.com>
2021-01-07 03:42:42 -0400

    Making single chapter fail better

--- single_chapter.rb
+++ single_chapter.rb
@@ -7,10 +7,11 @@ require 'emoji'
 @url = ARGV[0]
 # @url = "https://palewebserial.wordpress.com/2021/01/02/gone-ahead-7-9/"
 @chap_class = SerialChapter::classFinder @url
+puts @chap_class
 begin
 	@chap = @chap_class.new @url
-rescue
-	retry
+rescue => error
+	puts error
 end
 
 File.open("tmp/#{@chap.title}.html","w") do |f|
