2b5762e2a9f8db462faa716e2ec91e6f3ede53bd
Ming Scott <ming.scott@gmail.com>
2021-07-15 19:42:08 -0300

    work on v3

--- rss_checkerv3.rb
+++ rss_checkerv3.rb
@@ -2,20 +2,11 @@ require 'json'
 require 'mail'
 require 'nokogiri'
 require 'open-uri'
-require 'concurrent'
-require_relative 'lib/rss-feed'
-include RssFeed
 
-threads = []
-feeds = []
-File.read("conf/feeds.json").yield_self do |text|
-	JSON.parse(text).yield_self do |feedhash|
-		feedhash.map(&:last).each do |feed|
-			threads << Thread.new do
-				feeds << RssFeed::Feed.new(feed).to_a_of_h
-			end
-		end
-	end
-end
-threads.map(&:join)
-puts feeds[0]
+$threads = []
+POOL_SIZE = 10
+
+jobs = Queue.new
+dormant = Queue.new
+
+checkers = 
